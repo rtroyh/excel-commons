5f4b3a85003935f071ef908517faa1332bf0c2a4
rtroy <rtroy@13f10598-5c34-4a87-bf25-6f6a81c8df7a>
2013-09-17 00:25:31 +0000

    REFACTORING
    git-svn-id: https://gathercl.svn.cloudforge.com/java/maven/excelcommons@9397 13f10598-5c34-4a87-bf25-6f6a81c8df7a

---
+++ src/main/java/com/gather/core/sheet/ISheetConfigurator.java
@@ -0,0 +1,14 @@
+package com.gather.core.sheet;
+
+import org.apache.poi.ss.usermodel.Sheet;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rodrigotroy
+ * Date: 9/16/13
+ * Time: 7:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ISheetConfigurator {
+    public void applyConfig(Sheet sheet);
+}
