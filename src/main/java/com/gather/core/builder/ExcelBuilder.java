7b525df2948aee93c6fad6ee89b707762b91cf90
rtroy <rtroy@13f10598-5c34-4a87-bf25-6f6a81c8df7a>
2013-09-30 17:15:26 +0000

    REFACTORING
    git-svn-id: https://gathercl.svn.cloudforge.com/java/maven/excelcommons@9438 13f10598-5c34-4a87-bf25-6f6a81c8df7a

---
+++ src/main/java/com/gather/core/builder/ExcelBuilder.java
@@ -0,0 +1,61 @@
+package com.gather.core.builder;
+
+import com.gather.core.sheet.ISheetCreator;
+import com.gather.core.sheet.ISheetPopulator;
+import com.gather.core.workbook.IWorkbookCreator;
+import org.apache.log4j.Logger;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * $ Project: excelcommons
+ * User: rodrigotroy
+ * Date: 9/30/13
+ * Time: 12:37 PM
+ */
+public class ExcelBuilder {
+    private static final Logger LOG = Logger.getLogger(ExcelBuilder.class);
+
+    private ISheetCreator sheetCreator;
+    private IWorkbookCreator workbookCreator;
+    private ISheetPopulator bodySheetPopulator;
+    private ISheetPopulator headerSheetPopulator;
+
+    public ExcelBuilder(ISheetCreator sheetCreator,
+                        IWorkbookCreator workbookCreator,
+                        ISheetPopulator bodySheetPopulator,
+                        ISheetPopulator headerSheetPopulator) {
+        this.sheetCreator = sheetCreator;
+        this.workbookCreator = workbookCreator;
+        this.bodySheetPopulator = bodySheetPopulator;
+        this.headerSheetPopulator = headerSheetPopulator;
+    }
+
+    public void createExcel() {
+        if (sheetCreator != null && workbookCreator != null) {
+            workbookCreator.createWorkbook();
+
+            sheetCreator.createSheet(workbookCreator.getWorkbook());
+
+            headerSheetPopulator.populate(sheetCreator.getSheet());
+
+            bodySheetPopulator.populate(sheetCreator.getSheet());
+        }
+    }
+
+    private ByteArrayOutputStream getStream() {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+        try {
+            workbookCreator.getWorkbook().write(os);
+        } catch (IOException e) {
+            LOG.error(e.getMessage());
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+        }
+
+        return os;
+    }
+}
