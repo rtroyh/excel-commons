04e79a59503f59f10796e00970aed84e5967b011
rtroy <rtroy@13f10598-5c34-4a87-bf25-6f6a81c8df7a>
2013-10-01 13:59:27 +0000

    NUEVA IMPLEMENTACION ISheetPopulator
    git-svn-id: https://gathercl.svn.cloudforge.com/java/maven/excelcommons@9446 13f10598-5c34-4a87-bf25-6f6a81c8df7a

---
+++ src/main/java/com/gather/core/sheet/BasicSheetPopulator.java
@@ -0,0 +1,64 @@
+package com.gather.core.sheet;
+
+import com.gather.gathercommons.model.IDataTableModel;
+import com.gather.gathercommons.util.Validator;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.*;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * $ Project: excelcommons
+ * User: rodrigotroy
+ * Date: 10/1/13
+ * Time: 10:58 AM
+ */
+public class BasicSheetPopulator implements ISheetPopulator {
+    private IDataTableModel model;
+    private CellStyle cellStyleHeader;
+
+    public BasicSheetPopulator(IDataTableModel model) {
+        this.model = model;
+    }
+
+    @Override
+    public void populate(Sheet sheet) {
+        short rowIndex = 0;
+        short columnIndex = 0;
+
+        final Row headerRow = sheet.createRow(rowIndex);
+        final CellStyle cellStyle = getCellStyleHeader(sheet.getWorkbook());
+
+        for (List<Object> header : model.getHeaders()) {
+            if (!header.get(1).equals(5) && header.get(4).equals(1)) {
+                Cell cell = headerRow.createCell(columnIndex);
+                cell.setCellStyle(cellStyle);
+
+                String title = Validator.validateString(header.get(0)) ? header.get(0).toString() : " ";
+
+                cell.setCellValue(title);
+                columnIndex++;
+            }
+        }
+    }
+
+    private CellStyle getCellStyleHeader(Workbook wb) {
+        if (this.cellStyleHeader == null) {
+            this.cellStyleHeader = wb.createCellStyle();
+            this.cellStyleHeader.setAlignment(CellStyle.ALIGN_CENTER);
+            this.cellStyleHeader.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
+            this.cellStyleHeader.setFillForegroundColor(HSSFColor.DARK_BLUE.index);
+            this.cellStyleHeader.setFillPattern(CellStyle.SOLID_FOREGROUND);
+
+            Font font = wb.createFont();
+            font.setFontHeightInPoints((short) 11);
+            font.setBoldweight(Font.BOLDWEIGHT_BOLD);
+            font.setColor(HSSFColor.WHITE.index);
+
+            this.cellStyleHeader.setFont(font);
+        }
+
+        return this.cellStyleHeader;
+    }
+}
